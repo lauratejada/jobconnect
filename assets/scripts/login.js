5952c7676b2aebfaa8b5a139d50bd84c12109ec8
LauraTejada <laura2191@hotmail.com>
2023-04-13 23:33:26 -0500

    storage validation

---
+++ assets/scripts/login.js
@@ -0,0 +1,30 @@
+'use strict';
+
+localStorage.setItem('username', 'lauratejada');
+localStorage.setItem('password', '123456');
+
+const btnLogin = document.querySelector('#btn-login');
+
+console.log(document.location);
+console.log(document.location.origin);
+console.log(document.location.pathname);
+//checking
+function check(){
+    const storedName = localStorage.getItem('username');
+    const storedPass = localStorage.getItem('password');
+
+    const userName = document.querySelector('#userName');
+    const userPass = document.querySelector('#userPassword');
+    const message = document.querySelector('.message-invalid');
+
+    if(userName.value == storedName && userPass.value == storedPass){
+        console.log('You are logged in.');
+
+       window.location.assign(`${document.location.origin}/home.html`);
+    }else{
+        console.log('Error on login');
+        message.style.display = 'block';
+    }
+}
+
+btnLogin.addEventListener('click', check); 
