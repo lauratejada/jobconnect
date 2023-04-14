4483ab13bac49d2445bced37136012195be60e45
LauraTejada <laura2191@hotmail.com>
2023-04-14 00:21:42 -0500

    url redirected

--- assets/scripts/login.js
+++ assets/scripts/login.js
@@ -10,22 +10,28 @@ console.log(document.location.origin);
 console.log(document.location.pathname);
 const urlLogin = document.location.href + 'home.html';
 //checking
-function check(){
+function validateLogin(){
     const storedName = localStorage.getItem('username');
     const storedPass = localStorage.getItem('password');
+    console.log(storedName);
+    console.log(storedPass);
 
     const userName = document.querySelector('#userName');
     const userPass = document.querySelector('#userPassword');
     const message = document.querySelector('.message-invalid');
 
-    if(userName.value == storedName && userPass.value == storedPass){
+    console.log(userName.value);
+    console.log(userPass.value);
+
+    if((userName.value == storedName) && (userPass.value == storedPass)){
         console.log('You are logged in.');
 
-       window.location.assign(urlLogin);
+       //window.location.assign(urlLogin);
+        window.location.href = 'home.html';
     }else{
         console.log('Error on login');
         message.style.display = 'block';
     }
 }
 
-btnLogin.addEventListener('click', check); 
+btnLogin.addEventListener('click', validateLogin); 
